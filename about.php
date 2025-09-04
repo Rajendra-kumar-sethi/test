ca03c0189dcf4b6b3f28944315cab24a0845f0ea
rajendra <rajendrakumarsethi02@gmail.com>
2025-09-04 20:34:28 +0530

    i am added h3

--- about.php
+++ about.php
@@ -9,5 +9,7 @@
     <h1>Hello i am About page</h1>
     <p style="color:red;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil rem provident accusantium illo sapiente labore officiis quidem molestiae. Quis debitis mollitia, consectetur dolores modi voluptate ab a accusamus nesciunt facilis nihil, obcaecati consequuntur repellat architecto quasi numquam tempore dicta voluptas optio vel consequatur. Vero qui beatae ducimus rerum corporis totam maxime repellendus odit suscipit ullam voluptatibus dolore quisquam, inventore, quasi perferendis nobis eum! Recusandae repudiandae similique vitae eaque eveniet voluptatum repellat saepe qui laudantium iusto, facere error dolores veniam atque. Illo, laborum dolorem voluptas hic ex incidunt animi harum beatae ad. Fuga voluptas atque accusantium dolorem reiciendis error numquam! Aliquid?</p>
     <h2>I am h2 heading</h2>
+    <h3>i am h3</h3>
+    
 </body>
 </html>
